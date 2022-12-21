5d7658e7b21e62f167308feab16d8f0c506ca53c
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2022-12-21 22:49:17 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,37 @@
 /**
+ * leet - a function that encodes a string into 1337
  *
- *  * _strcmp - a function that compares two strings
+ * @s: string input
  *
- *   *
- *
- *    * @s1: string 1 input to compare
- *
- *     * @s2: against this other string 2
- *
- *      *
- *
- *       * Return: 0 if s1 and s2 are equal
- *
- *        *        negative integer if the stopping character
- *
- *         *                in @s1 was less than the stopping
- *
- *          *                character in @s2
- *
- *           *        positive integer if the stopping character
- *
- *            *                in @s1 was greater than the stopping
- *
- *             *                character in @s2
+ * Return: @s
+*/
+
+char *leet(char *s)
+{
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	/*iterate values in array s*/
+	while (s[c] != '\0')
+	{
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		*/
+		for (i = 0; i < 5; i++)
+		{
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
+}
