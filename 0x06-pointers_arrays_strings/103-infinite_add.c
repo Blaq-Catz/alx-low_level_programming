8212d666c6c8fadac98fb6953ccafb8abed2ea79
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2022-12-21 22:59:58 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,55 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * rot13 - encoder rot13
- *
- *   * @s: pointer to string params
- *
- *    *
- *
- *     * Return: *s
- *
- *      */
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
+ */
 
-
-
-char *rot13(char *s)
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-		int i;
-
-			int j;
-
-				char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-					char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-
-
-						for (i = 0; s[i] != '\0'; i++)
-
-								{
-
-											for (j = 0; j < 52; j++)
-
-														{
-
-																		if (s[i] == data1[j])
-
-																						{
-
-																											s[i] = datarot[j];
-
-																															break;
-
-																																		}
+	while (*(n1 + c1) != '\0')
+	c1++;
+	while (*(n2 + c2) != '\0')
+	c2++;
+	if (c1 >= c2)
+	bg = c1;
+	else
+	bg = c2;
+	if (size_r <= bg + 1)
+	return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+	op = dr1 + dr2 + add;
+	if (op >= 10)
+	add = op / 10;
+	else
+	add = 0;
+	if (op > 0)
+	*(r + bg) = (op % 10) + 48;
+	else
+	*(r + bg) = '0';
+	if (c1 > 0)
+	c1--, dr1 = *(n1 + c1) - 48;
+	else
+	dr1 = 0;
+	if (c2 > 0)
+	c2--, dr2 = *(n2 + c2) - 48;
+	else
+	dr2 = 0;
+	bg--, size_r--;
+	}
+	if (*(r) == '0')
+	return (r + 1);
+	else
+	return (r);
+}
