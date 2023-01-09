d61a155f5ab453a6f5183782b328dea4140b94b2
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-01-09 12:46:24 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,47 +1,45 @@
 #include <stdlib.h>
-
 #include "main.h"
 
-
-
 /**
+ * **alloc_grid - creates a two dimensional array of ints
+ * @width: width of the matrix
+ * @height: height of the matrix
  *
- *  * *create_array - creates an array of chars,
- *
- *   * and initializes it with a specific char
- *
- *    * @size: size of the array to create
- *
- *     * @c: char to initialize the array c
- *
- *      *
- *
- *       * Return: pointer to the array (Success), NULL (Error)
- *
- *        */
-
-char *create_array(unsigned int size, char c)
-
+ * Return: pointer to the created matrix (Success)
+ * or NULL (Error)
+ */
+int **alloc_grid(int width, int height)
 {
+	int **arr;
+	int i, j;
 
-		char *p;
+	if (height <= 0 || width <= 0)
+		return (NULL);
 
-			unsigned int i = 0;
+	arr = (int **) malloc(sizeof(int *) * height);
 
+	if (arr == NULL)
+		return (NULL);
 
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
+			return (NULL);
+		}
+	}
 
-				if (size == 0)
-
-							return (NULL);
-
-
-
-					p = (char *) malloc(sizeof(char) * size);
-
-
-
-						if (p == NULL)
-
-									return (0);
-
-
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
