36999d910ad0c962c48abcba5a7535e987662e16
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-01-12 12:39:31 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,67 +1,51 @@
 #include <stdlib.h>
 
-#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area
+*/
 
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
 
 /**
+ * _calloc - a function that allocates
+ *           memory for an array using malloc
  *
- *  * *create_array - creates an array of chars,
+ *           It is basically the equivalent to
+ *           malloc followed by memset
  *
- *   * and initializes it with a specific char
+ * @nmemb: size of array
+ * @size: size of each element
  *
- *    * @size: size of the array to create
- *
- *     * @c: char to initialize the array c
- *
- *      *
- *
- *       * Return: pointer to the array (Success), NULL (Error)
- *
- *        */
+ * Return: pointer with new allocated memory
+ *         or NULL if it fails
+*/
 
-char *create_array(unsigned int size, char c)
-
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
 
-		char *p;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-			unsigned int i = 0;
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
 
+	_memset(p, 0, nmemb * size);
 
-
-				if (size == 0)
-
-							return (NULL);
-
-
-
-					p = (char *) malloc(sizeof(char) * size);
-
-
-
-						if (p == NULL)
-
-									return (0);
-
-
-
-							while (i < size)
-
-									{
-
-												*(p + i) = c;
-
-														i++;
-
-															}
-
-
-
-								*(p + i) = '\0';
-
-
-
-									return (p);
-
+	return (p);
 }
