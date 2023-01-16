7f43a80fed746398ab8534757edb1021127c78e3
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-01-16 14:23:57 +0100

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H
+#ifndef _PI_H
+#define _PI_H
 
-#define _OBJECT_LIKE_MACRO_H
+#define PI 3.14159265359
 
-
-
-#define SIZE 1024
-
-
-
-#endif /* _OBJECT_LIKE_MACRO_H */
+#endif /* _PI_H */
