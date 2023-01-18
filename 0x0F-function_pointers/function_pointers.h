381929c9532972e8e4feab7ca2a746807e01a8b9
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-01-18 19:03:19 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,19 @@
-#include <stdlib.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <time.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-#include <stdio.h>
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
+#endif /* FUNCTION_POINTERS_H */
