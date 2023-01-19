6ed8799dccc54c61e9437909d998a85704a9ceaf
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-01-19 14:54:21 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,33 +1,51 @@
-#include <stdarg.h>
-
-
+#include "variadic_functions.h"
 
 /**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- *  * sum_them_all - adds all its parameters
- *
- *   *
- *
- *    * @n: start of input variables
- *
- *     *
- *
- *      * Return: the sum
- *
- *      */
-
-
-
-int sum_them_all(const unsigned int n, ...)
-
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
 {
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-		va_list ap;
-
-			unsigned int param, sum = 0;
-
-
-
-				/* initialize the argument list from the start */
-
-				va_start(ap, n);
+	va_start(valist, format);
+	while (format && format[i])
+	{
+	j = 0;
+	while (t_arg[j])
+	{
+	if (format[i] == t_arg[j] && c)
+	{
+	printf(", ");
+	break;
+	} j++;
+	}
+	switch (format[i])
+	{
+	case 'c':
+		printf("%c", va_arg(valist, int)), c = 1;
+		break;
+	case 'i':
+		printf("%d", va_arg(valist, int)), c = 1;
+		break;
+	case 'f':
+		printf("%f", va_arg(valist, double)), c = 1;
+		break;
+	case 's':
+		str = va_arg(valist, char *), c = 1;
+		if (!str)
+		{
+		printf("(nil)");
+		break;
+		}
+		printf("%s", str);
+		break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
+}
